ad1606b16cf5bb46b02aacd1a71cb506882744c3
KunalPusdekar <kunalpusdekar16@gmail.com>
2025-04-14 09:50:31 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,6 @@
-"use client";
-import Hero from "./components/hero-section/Hero";
-import Skills from "./components/skill-section/Skills";
-import Resume from "./components/resume-section/Resume";
-import About from "./components/about-section/About";
-import Contact from "./components/contact+footer/Contact";
-import Footer from "./components/contact+footer/Footer";
-import Work from "./components/work-section/work";
+// app/page.tsx
+import Home from "./Home";
 
-
-
-export default function Home() {
- 
-
-  return (
-    <main className="overflow-x-hidden sm:overflow-x-visible">
-      <Hero />
-      <Skills />
-      <Resume />
-      <Work />
-      <About />
-      <Contact />
-      <Footer />
-    </main>
-  );
+export default function Page() {
+  return <Home />;
 }
